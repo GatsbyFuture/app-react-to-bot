38b98d65f7ac6424022f21b0ffe706d516f0d536
Eldorbek <raxmatoveldor37@gmail.com>
2023-04-09 13:31:04 +0500

    added main button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import { useEffect } from 'react';
 import './App.css';
-// window.Telegram.WebApp.
-const tg = window.Telegram.WebApp;
+import { useTelegram } from './hooks/useTelegram';
+
 function App() {
+
+  const { onToggleButton } = useTelegram();
 
   useEffect(() => {
     tg.ready();
   }, []);
 
-  const onClose = () => {
-    tg.close();
-  };
 
   return (
     <div className="App">
       work
-      <button onClick={onClose}>Close</button>
+      <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
 }
