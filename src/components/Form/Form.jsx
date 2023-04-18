4f984690ea68fc335abdb6c255ad8efffff1ac81
Eldorbek <raxmatoveldor37@gmail.com>
2023-04-18 18:43:16 +0500

    added any style for date input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -113,7 +113,6 @@ const Form = () => {
             <div className="date-input">
                 <label htmlFor="dataOfBirthday">Tug'ulgan yilingizni kiriting</label>
                 <input
-                    hidden
                     className={"input"}
                     type="date"
                     placeholder='Tugulgan kun'
